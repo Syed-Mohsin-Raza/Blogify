3890b1c08f9b8185333738de229a3ba6a78342d5
Syed-Mohsin-Raza <raza.mohsin1441@gmail.com>
2026-05-25 20:00:55 +0500

    Commenting Functionalty added.

---
+++ models/comment.js
@@ -0,0 +1,20 @@
+const { Schema, model } = require('mongoose');
+
+const commentSchema =  new Schema({
+    content: {
+        type: String,
+        required: true,
+    },
+    blogId: {
+        type: Schema.Types.ObjectId,
+        ref: "blog",
+        required: true,
+    },
+    createdBy: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+}, {timestamps: true});
+
+module.exports = model('Comment', commentSchema);
